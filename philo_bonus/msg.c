1d63b888e9d16d38533690d834282ac0564b5aa8
taehokim <taehokim@student.42seoul.kr>
2021-06-14 20:15:32 +0900

    moved

--- philo_bonus/msg.c
+++ philo_bonus/msg.c
@@ -6,54 +6,56 @@
 /*   By: taehokim <taehokim@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/02 13:44:36 by taehokim          #+#    #+#             */
-/*   Updated: 2021/06/11 08:46:27 by taehokim         ###   ########.fr       */
+/*   Updated: 2021/06/14 19:53:50 by taehokim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
 #include "philosophers.h"
 
 void	msg_take_fork(uint64_t t, long id)
 {
-	pthread_mutex_lock(&vars()->somebody_died_lock);
-	if (!vars()->somebody_died)
+	sw(vars()->msg_sem, "msg_sem wait failed");
+	if (!is_somebody_died())
 	{
 		put_msg(t, id, ANSI_COLOR_BLUE, "has taken a fork");
 		put_msg(t, id, ANSI_COLOR_BLUE, "has taken a fork");
 	}
-	pthread_mutex_unlock(&vars()->somebody_died_lock);
+	sp(vars()->msg_sem, "msg_sem wait failed");
 }
 
 void	msg_eat(uint64_t t, long id)
 {
-	pthread_mutex_lock(&vars()->somebody_died_lock);
-	if (!vars()->somebody_died)
+	sw(vars()->msg_sem, "msg_sem wait failed");
+	if (!is_somebody_died())
 		put_msg(t, id, ANSI_COLOR_CYAN, "is eating");
-	pthread_mutex_unlock(&vars()->somebody_died_lock);
+	sp(vars()->msg_sem, "msg_sem wait failed");
 }
 
 void	msg_sleep(uint64_t t, long id)
 {
-	pthread_mutex_lock(&vars()->somebody_died_lock);
-	if (!vars()->somebody_died)
+	sw(vars()->msg_sem, "msg_sem wait failed");
+	if (!is_somebody_died())
 		put_msg(t, id, ANSI_COLOR_MAGENTA, "is sleeping");
-	pthread_mutex_unlock(&vars()->somebody_died_lock);
+	sp(vars()->msg_sem, "msg_sem wait failed");
 }
 
 void	msg_think(uint64_t t, long id)
 {
-	pthread_mutex_lock(&vars()->somebody_died_lock);
-	if (!vars()->somebody_died)
+	sw(vars()->msg_sem, "msg_sem wait failed");
+	if (!is_somebody_died())
 		put_msg(t, id, ANSI_COLOR_YELLOW, "is thinking");
-	pthread_mutex_unlock(&vars()->somebody_died_lock);
+	sp(vars()->msg_sem, "msg_sem wait failed");
 }
 
 void	msg_die(uint64_t t, long id)
 {
-	pthread_mutex_lock(&vars()->somebody_died_lock);
-	if (!vars()->somebody_died)
+	sw(vars()->msg_sem, "msg_sem wait failed");
+	if (!is_somebody_died())
 	{
-		vars()->somebody_died = 1;
 		put_msg(t, id, ANSI_COLOR_RED, "died");
+		su(SN_DIED, "died unlink failed");
+		exit(0);
 	}
-	pthread_mutex_unlock(&vars()->somebody_died_lock);
+	sp(vars()->msg_sem, "msg_sem wait failed");
 }
